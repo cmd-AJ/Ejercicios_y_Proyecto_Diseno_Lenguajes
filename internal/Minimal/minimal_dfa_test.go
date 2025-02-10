fbd0232e3722ada15d6ccff78748649bd7ce8eb6
cmd-AJ <Andrewlol774@gmail.com>
2025-02-10 16:08:30 -0600

    finish v1 of minimize but need to put the minimize to dfa

--- internal/Minimal/minimal_dfa_test.go
+++ internal/Minimal/minimal_dfa_test.go
@@ -3,7 +3,6 @@
 package Minimal
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -20,38 +19,59 @@ func Test_Check_repeat(t *testing.T) {
 	// print(tablucha["G"]["G"])
 
 	// showkeys(tablucha)
-	var tuplas = lista_a_marcar_antes_finals(tablucha)
+	var tuplas = Lista_a_marcar_antes_finals(tablucha)
 
-	tuplas = recorrer_x_tupla(tuplas, table, tablucha)
+	tuplas = Recorrer_x_tupla(tuplas, table, tablucha)
 	//Recorrer solo una vez para verificar si no hubo algo faltante
-	tuplas = recorrer_x_tupla(tuplas, table, tablucha)
+	tuplas = Recorrer_x_tupla(tuplas, table, tablucha)
 
 	for outerKey, innerMap := range tablucha {
 		// Iterate over the inner map
 		for innerKey := range innerMap {
 			// Print the outer key, inner key, and value
-			fmt.Printf("OuterKey: %s, InnerKey: %s, Value: %v\n", outerKey, innerKey, innerMap[innerKey])
+			if innerMap[innerKey] == false {
+				Replacex_index(outerKey, innerKey, table)
+			}
 		}
 	}
 
-	//recorrer false
+	println("Index in X")
+	for f := 0; f < len(table.x_index); f++ {
+		print(" ", table.x_index[f])
+	}
+	println()
+	println("Index in Y")
+	for f := 0; f < len(table.y_index); f++ {
+		print(" ", table.y_index[f])
+	}
+	println()
 
-}
-
-func showkeys(mappings map[string]map[string]bool) {
-
-	for outerKey, innerMap := range mappings {
-		fmt.Printf("Outer Key: %s\n", outerKey)
-
-		// Loop through the inner map
-		for innerKey, value := range innerMap {
-			fmt.Printf("  Inner Key: %s, Value: %v\n", innerKey, value)
+	//Ver si esta bien
+	for i := 0; i < len(table.Table_2D); i++ {
+		for e := 0; e < len(table.Table_2D[e]); e++ {
+			print("	" + table.Table_2D[i][e] + " 	")
 		}
+		println()
 	}
 
 }
 
-func lista_a_marcar_antes_finals(mappings map[string]map[string]bool) []Tuple {
+func Replacex_index(outerKey string, innerkey string, tabla Table) {
+	for i := 0; i < len(tabla.Table_2D); i++ {
+		for e := 0; e < len(tabla.Table_2D[i]); e++ {
+			if tabla.Table_2D[i][e] == outerKey || tabla.Table_2D[i][e] == innerkey {
+				tabla.Table_2D[i][e] = (innerkey + outerKey)
+			}
+		}
+	}
+	for i := 0; i < len(tabla.x_index); i++ {
+		if tabla.x_index[i] == outerKey || tabla.x_index[i] == innerkey {
+			tabla.x_index[i] = (innerkey + outerKey)
+		}
+	}
+}
+
+func Lista_a_marcar_antes_finals(mappings map[string]map[string]bool) []Tuple {
 
 	var tuples []Tuple
 
@@ -65,7 +85,7 @@ func lista_a_marcar_antes_finals(mappings map[string]map[string]bool) []Tuple {
 	return tuples
 }
 
-func recorrer_x_tupla(tuplas []Tuple, tabla Table, mappings map[string]map[string]bool) []Tuple {
+func Recorrer_x_tupla(tuplas []Tuple, tabla Table, mappings map[string]map[string]bool) []Tuple {
 	var newTuplesAdded bool
 	var initialLength = len(tuplas)
 
@@ -74,22 +94,15 @@ func recorrer_x_tupla(tuplas []Tuple, tabla Table, mappings map[string]map[strin
 		for d := 0; d < len(tabla.Table_2D)-1; d++ {
 			for k := d + 1; k < len(tabla.Table_2D); k++ {
 				tuple := Tuple{OuterKey: tabla.Table_2D[d][s], InnerKey: tabla.Table_2D[k][s]}
-				if tupleExists(tuplas, tuple) {
-					newTuple := Tuple{OuterKey: tabla.x_index[d], InnerKey: tabla.x_index[k]}
-					if !tupleExists(tuplas, newTuple) {
-						tuplas = append(tuplas, newTuple)
-						mappings[newTuple.OuterKey][newTuple.InnerKey] = true
-						newTuplesAdded = true
-
-					}
+				if TupleExists(tuplas, tuple) {
+					mappings[tabla.x_index[d]][tabla.x_index[k]] = true
 				}
 
 			}
 		}
 	}
 	if newTuplesAdded && len(tuplas) > initialLength {
-		// Only update mappings if the tuple was actually added
-		recorrer_x_tupla(tuplas, tabla, mappings)
+		Recorrer_x_tupla(tuplas, tabla, mappings)
 
 		return tuplas
 	}
@@ -98,9 +111,9 @@ func recorrer_x_tupla(tuplas []Tuple, tabla Table, mappings map[string]map[strin
 
 }
 
-func tupleExists(tuples []Tuple, target Tuple) bool {
+func TupleExists(tuples []Tuple, target Tuple) bool {
 	for _, t := range tuples {
-		if t == target { // Direct comparison since all fields are comparable
+		if t == target {
 			return true
 		}
 	}
@@ -109,14 +122,17 @@ func tupleExists(tuples []Tuple, target Tuple) bool {
 
 // Debe llenar LLenar la tabla de espacios vacios
 func Initilize_table(table Table) map[string]map[string]bool {
-
+	var contador = 0
 	tabla := make(map[string]map[string]bool)
 	for i := range len(table.x_index) - 1 {
 
 		tabla[table.x_index[i]] = make(map[string]bool)
-		for e := range len(table.x_index) {
-			println(table.x_index[e])
-			if e > 0 {
+		//Agregar un if que si es mayor al contador
+
+		for e := len(table.x_index) - 1; e >= 1; e-- {
+
+			if 0 < e-contador {
+				tabla[table.x_index[i]][table.x_index[e]] = false
 				//Este es llenar si son estados finales con true
 				if !(table.finals[table.x_index[i]] && table.finals[table.x_index[e]]) {
 					if table.finals[table.x_index[i]] == true {
@@ -129,6 +145,7 @@ func Initilize_table(table Table) map[string]map[string]bool {
 
 			}
 		}
+		contador++
 	}
 
 	return tabla
