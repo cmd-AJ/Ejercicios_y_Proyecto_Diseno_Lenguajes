d188807547c56bf91413814a186edc70bc04c9dc
DanielRasho <ray22933@uvg.edu.gt>
2025-02-09 23:28:02 -0600

    test: create go test for translate regex operators

---
+++ internal/Postfix/postfix_test.go
@@ -0,0 +1,88 @@
+package postfix
+
+import (
+	"slices"
+	"strings"
+	"testing"
+)
+
+func areSlicesEqual(t *testing.T, response []Symbol, expect []string) {
+	value := ""
+	for _, v := range response {
+		value += v.value
+	}
+
+	if len(response) < len(expect) {
+		t.Fatalf("Response has less characters than expected. Has %d, %d given. %s", len(response), len(expect), value)
+	} else if len(response) > len(expect) {
+		t.Fatalf("Response has more characters than expected. Has %d, %d given. %s", len(response), len(expect), value)
+	}
+	for i, expected := range expect {
+		if response[i].value != expected {
+			t.Fatalf("Characters not match, Given string %s", value)
+		}
+	}
+}
+
+func TestEscapedSymbols(t *testing.T) {
+	symbol, _ := convertToSymbols("\\+")
+	expect := Symbol{value: "+", isOperator: false, precedence: 60}
+
+	if symbol[0] != expect {
+		t.Fatalf("Symbols %s was not ESCAPED.", (&symbol[0]).value)
+	}
+}
+func TestFindSubExpresionIndex(t *testing.T) {
+	symbols, _ := convertToSymbols("(a+b)c")
+	start, end := getSubExpresionIndex(symbols, 4, "(", ")")
+	t.Logf("start: %d end: %d \n", start, end)
+}
+
+func TestClassInterchangeClasses(t *testing.T) {
+	symbols, _ := convertToSymbols("a-cz1-3z")
+	answer := interchangeClasses(symbols)
+	var sb strings.Builder
+	for _, a := range answer {
+		sb.WriteString(a.value)
+	}
+	t.Logf(sb.String())
+}
+
+func TestClassInterchangeClassesEscaped(t *testing.T) {
+	symbols, _ := convertToSymbols("a-c\\]d-e")
+	answer := interchangeClasses(symbols)
+	var sb strings.Builder
+	for _, a := range answer {
+		sb.WriteString(a.value)
+	}
+	t.Logf(sb.String())
+}
+
+func TestClassInterchangeOptional(t *testing.T) {
+	symbols, _ := convertToSymbols("(ab)")
+	answer := interchangeOptional(symbols)
+	slices.Reverse(answer)
+	expect := strings.Split("((ab)|ε)", "")
+	areSlicesEqual(t, answer, expect)
+}
+
+func TestClassPositiveLock(t *testing.T) {
+	symbols, _ := convertToSymbols("(ab)")
+	answer := interchangePositiveLock(symbols)
+	slices.Reverse(answer)
+	expect := strings.Split("((ab)(ab)*)", "")
+	areSlicesEqual(t, answer, expect)
+}
+func TestTranslateToPrimitivesNested(t *testing.T) {
+	symbols, _ := convertToSymbols("(a(j)+)")
+	answer := convertToPrimitiveOperators(symbols)
+	expect := strings.Split("(a((j)(j)*))", "")
+	areSlicesEqual(t, answer, expect)
+}
+
+func TestTranslateToPrimitives(t *testing.T) {
+	symbols, _ := convertToSymbols("(ab)?[1-3M-O](a(j)+)")
+	answer := convertToPrimitiveOperators(symbols)
+	expect := strings.Split("((ab)|ε)(1|2|3|M|N|O)(a((j)(j)*))", "")
+	areSlicesEqual(t, answer, expect)
+}
